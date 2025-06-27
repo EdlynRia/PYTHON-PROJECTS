c7d7c2fbd342494fd7e85dc197ddb1295ba95833
EdlynRia <edlynria.cx@gmail.com>
2025-06-27 21:07:49 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,6 @@
+u=int(input("enter your age"))
+print(u)
+if u>17:
+    print("you can vote")
+else:
+    print("you cannot vote")
---
+++ positive or negative numbers.py
@@ -0,0 +1,6 @@
+i=int(input("enter a number"))
+print(i)
+if i<0:
+    print("it is a negative number")
+else:
+    print("it is a positive number")
