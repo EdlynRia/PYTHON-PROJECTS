c04d34762d758a2221b444311e150ec8a7946479
EdlynRia <edlynria.cx@gmail.com>
2025-07-20 13:57:15 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,23 @@
+h=0
+print("i have two ways to get in but one house,who am i?")
+print("A.apartment \nB.nose \nC.park \nD.swimming pool")
+g=input("what is the answer?")
+if g=="b":
+    print("correct answer congrates!!!!!!!!!!")
+    h=h+200
+    print("your score is 200!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",h)
+else:
+    print("sorry you have got it wrong") 
+    h=h-200
+    print("your score is 0",h)
+print("what is indias national animal?")
+print("A.rat B.cat C.peacock D.dog")
+j=input("what is the answer?")
+if j=="c":
+    print("correct answer congrates!!!!!!!!!!")
+    h=h+200
+    print("your score is 200!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",h)
+else:
+    print("sorry you have got it wrong") 
+    h=h-200
+    print("your score is 0",h)
